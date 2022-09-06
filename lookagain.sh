5b176834a05dc7925bf28ad21e8b666a74ebc766
choang# <charhoang@outlook.com>
2022-09-06 11:20:17 +0100

    My very first commit

--- lookagain.sh
+++ lookagain.sh
@@ -1,4 +1,4 @@
-find . -type f -iname "*.sh" -execdir sh -c 'printf "%s\n" "${0%.*}"' {} ';' | tac | cut -d "/" -f2 | cut -d "." -f1
+find . -type f -iname "*.sh" -execdir sh -c 'printf "%s\n" "${0%.*}"' {} ';' | sort -r | cut -d "/" -f2 | cut -d "." -f1
 
 
 
