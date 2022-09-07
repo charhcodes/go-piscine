13dc7a97b57a4e9a08fc7df339d52331c2b1f4c8
choang <charhoang@outlook.com>
2022-09-07 14:25:57 +0100

    go commit

--- printcomb.go
+++ printcomb.go
@@ -1,8 +1,8 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
-func main() {
+func PrintComb() {
 	for a := '0'; a <= 9; a++ {
 		for b := a + 1; b <= 9; b++ {
 			for c := b + 1; c <= 9; c++ {
