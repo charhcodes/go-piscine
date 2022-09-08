7ac3099c5f630e1a43a1e456cf6136a99883abde
choang <charhoang@outlook.com>
2022-09-08 13:45:22 +0100

    go commit 2

---
+++ printstr.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintStr(s string) {
+	s := "Hello World!"
+
+	bob = []rune(s)
+	for i := 0; i < len(bob); i++ {
+		z01.PrintRune(bob[i])
+	}
+	z01.PrintRune('\n')
+}
