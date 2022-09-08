121227b968c1a50d188d2b6167dbe6cc3a54ed28
choang <charhoang@outlook.com>
2022-09-08 12:28:46 +0100

    go commit 2

--- divmod.go
+++ divmod.go
@@ -1,14 +1,6 @@
 package piscine
 
 func DivMod(a int, b int, div *int, mod *int) {
-	a = 3
-	b = 4
-	d := 2
-
-	c := a + b
-	e := c / d
-	f := c % d
-
-	*div = e
-	*mod = f
+	*div = a / b
+	*mod = a % b
 }
