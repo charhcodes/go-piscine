7c9a58635fbe8ec54a89a9f39d88fea4528f058f
choang <charhoang@outlook.com>
2022-09-08 14:44:59 +0100

    go commit 2

--- strlen.go
+++ strlen.go
@@ -1,12 +1,6 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
 func StrLen(s string) int {
-	var x rune
-	x = []rune(s)
-
-	for i := 0; i < len(bob); i++ {
-		z01.PrintRune(i)
-	}
+	x := []rune(s)
+	return len(x)
 }
