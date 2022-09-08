227e7f2eae723dfd0ea222edee9a38ad63eff2df
choang <charhoang@outlook.com>
2022-09-08 14:05:38 +0100

    go commit 2

--- printstr.go
+++ printstr.go
@@ -4,8 +4,8 @@ import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
 	s = "Hello World!"
-	var bob rune
-	bob = []rune(s)
+	bob := []rune(s)
+
 	for i := '0'; i < len(bob); i++ {
 		z01.PrintRune(bob[i])
 	}
