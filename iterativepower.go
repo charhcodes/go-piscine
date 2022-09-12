39d8b120b3f78f487c1730166d14e08117e06da4
choang <charhoang@outlook.com>
2022-09-12 18:41:41 +0100

    4

--- iterativepower.go
+++ iterativepower.go
@@ -1,14 +1,15 @@
 package piscine
 
 func IterativePower(nb int, power int) int {
-	if power < 0 || nb == 0 {
+	result := nb
+
+	if power < 0 {
 		return 0
 	}
 	if power == 0 {
 		return 1
+	} for i := 1; i < power; i++ {
+		result = nb * result
 	}
-	if nb < 0 {
-		return nb * IterativePower(nb, power-1)
-	}
-	return 0
+	return result
 }
