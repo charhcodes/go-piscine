7c407000c7755a6bb0910f94d0690cb10d155b2b
choang <charhoang@outlook.com>
2022-09-12 16:53:09 +0100

    4

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -5,10 +5,10 @@ func RecursiveFactorial(nb int) int {
 	if nb < 0 || nb > 20 {
 		return 0
 	} else {
-		RecursiveFactorial(factorial * nb)
+		RecursiveFactorial(factorial*nb - 1)
 		if nb == 1 {
 			return factorial
 		}
 	}
-	return
+	return factorial
 }
