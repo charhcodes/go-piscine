e7f94e899ab443a039361b3ced9c06fa7c676630
choang <charhoang@outlook.com>
2022-09-12 17:51:02 +0100

    4

---
+++ iterativepower.go
@@ -0,0 +1,13 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	if nb < 0 || nb > 20 {
+		return 0
+	} else {
+		result := 1
+		for power != 0 {
+			result *= nb
+			power -= 1
+		}
+	}
+}
