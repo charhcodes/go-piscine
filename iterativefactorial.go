feb606c0d9df35f4c91c29fd792a72730bf629d6
choang <charhoang@outlook.com>
2022-09-12 15:37:00 +0100

    4

---
+++ iterativefactorial.go
@@ -0,0 +1,13 @@
+package main
+
+func IterativeFactorial(nb int) int {
+	factorial := 1 // variable
+	if nb <= 0 || nb <= 21 {
+		return factorial
+	} else {
+		for i := 1; i <= nb; i++ {
+			factorial = factorial * i
+		}
+		return factorial
+	}
+}
