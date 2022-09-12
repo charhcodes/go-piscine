a230d435050f8976fcee59dc0130dc150f933ee6
choang <charhoang@outlook.com>
2022-09-12 17:01:12 +0100

    4

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -3,8 +3,7 @@ package piscine
 func RecursiveFactorial(nb int) int {
 	if nb < 0 || nb > 20 {
 		return 0
-	}
-	if nb == 1 {
+	} else if nb == 1 {
 		return 1
 	} else {
 		return nb * RecursiveFactorial(nb-1)
