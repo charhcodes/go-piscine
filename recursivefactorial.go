f590ab25547b425247d13854a08124502d474895
choang <charhoang@outlook.com>
2022-09-12 16:26:45 +0100

    4

---
+++ recursivefactorial.go
@@ -0,0 +1,13 @@
+package main
+
+func RecursiveFactorial(nb int) int {
+	factorial := 1
+	if nb < 0 || nb > 20 {
+		return 0
+	} else {
+		RecursiveFactorial(factorial * i)
+		if nb == 1 {
+			return factorial
+		}
+	}
+}
