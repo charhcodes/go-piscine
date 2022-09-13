9f0d240f145459b5142b664c8ff7ca8cd00724b0
choang <charhoang@outlook.com>
2022-09-13 16:06:31 +0100

    5

--- alphacount.go
+++ alphacount.go
@@ -2,12 +2,13 @@ package piscine
 
 func AlphaCount(s string) int {
 	ConvRune := []rune(s)
+	counter := 0
 	for i := 0; i > len(s); i++ {
 		if (ConvRune <= 65 && ConvRune > 91) || (ConvRune <= 97 && ConvRune > 123) {
-			return ConvRune[i]
+			return counter
 		} else {
 			return 0
 		}
 	}
-	return len(ConvRune)
+	return counter
 }
