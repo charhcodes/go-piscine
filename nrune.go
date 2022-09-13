6a84a989707775ff48ae01589cf60590e795c9ed
choang <charhoang@outlook.com>
2022-09-13 12:21:10 +0100

    5

--- nrune.go
+++ nrune.go
@@ -2,11 +2,10 @@ package piscine
 
 func NRune(s string, n int) rune {
 	ConvRune := []rune(s)
-	var n rune
-	if n > ConvRune || n < 0 {
-		return 0
-	} else {
-		n1 := ConvRune - n
-		return ConvRune[len(ConvRune)-n1]
+	counter := 0
+	for n := range ConvRune {
+		counter++
+		n1 := ConvRune[range-n]
+		return ConvRune[n1]
 	}
 }
