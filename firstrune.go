fd6c9015b46fb8eaf6c8f5294b1f48af72efdda9
choang <charhoang@outlook.com>
2022-09-13 11:43:41 +0100

    4

--- firstrune.go
+++ firstrune.go
@@ -3,6 +3,6 @@ package piscine
 func FirstRune(s string) rune {
 	s = "Hello"
 	ConvRune := []rune(s)
-	ConvString := string(ConvRune[0:1])
-	return ConvString
+	result := ConvRune[0:1]
+	return result
 }
