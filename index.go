84cab940465cdab76924b81e264f6cb09000d894
choang <charhoang@outlook.com>
2022-09-13 18:06:35 +0100

    5

--- index.go
+++ index.go
@@ -5,7 +5,7 @@ func Index(s string, toFind string) int {
 	ChildString := []rune(toFind)
 	counter := 0
 
-	if ParentString > '0' && ChildString > '0' {
+	/*if ParentString > '0' && ChildString > '0' {
 		for i := 0; i < len(ParentString); i++ {
 			if ChildString[0] == ParentString[i] {
 				for j := 0; len(ChildString); j++ {
@@ -18,6 +18,20 @@ func Index(s string, toFind string) int {
 			} else {
 				return -1
 			}
+		}
+	}*/
+	for i := 0; i < len(ParentString); i++ {
+		if ChildString[0] == ParentString[i] {
+			counter++
+			for j := 0; len(ChildString); j++ {
+				if ChildString[j] == ParentString[i+j] {
+					counter++
+				} else {
+					return counter
+				}
+			}
+		} else {
+			return -1
 		}
 	}
 }
