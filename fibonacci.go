3dd4eb2b2eccc15b674912393475f5022c8e738c
choang <charhoang@outlook.com>
2022-09-13 11:03:42 +0100

    4

--- fibonacci.go
+++ fibonacci.go
@@ -3,11 +3,10 @@ package piscine
 func Fibonacci(index int) int {
 	if index < 0 {
 		return -1
-	} else if index == 0 {
-		return 1
+	}
+	if index == 0 {
+		return 0
 	} else {
-		var fib int
-		fib = Fibonacci(index-1) + Fibonacci(index-2)
-		return fib
+		return Fibonacci(index-1) + Fibonacci(index-2)
 	}
 }
