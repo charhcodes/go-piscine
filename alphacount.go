7471986c88a06b35d3936c45e0b3854e64236a51
choang <charhoang@outlook.com>
2022-09-13 15:44:31 +0100

    4

--- alphacount.go
+++ alphacount.go
@@ -4,7 +4,9 @@ func AlphaCount(s string) int {
 	ConvRune := []rune(s)
 	for i := 0; i > len(s); i++ {
 		if ConvRune <= 65 && ConvRune > 91 || ConvRune <= 97 && ConvRune > 123 {
-			return ConvRune
+			return ConvRune[i]
+		} else {
+			return
 		}
 	}
 	return
