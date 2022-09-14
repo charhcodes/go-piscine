d499a997460bcd4e08821ca3d7209ee0cf7ba39b
choang <charhoang@outlook.com>
2022-09-14 16:11:54 +0100

    5

--- capitalize.go
+++ capitalize.go
@@ -5,11 +5,11 @@ func Capitalize(s string) string {
 	converter := 'A' - 'a'
 
 	if len(StringRune) > 0 && IsLower(string(StringRune[0])) {
-		StringRune[0] = StringRune[0] + 32
+		StringRune[0] = StringRune[0] + converter
 	}
 
 	for i := 1; i < len(StringRune); i++ {
-		if StringRune[i-1] == " " || !IsAlpha(string(StringRune[i-1])) {
+		if StringRune[i-1] == 32 || !IsAlpha(string(StringRune[i-1])) {
 			if IsLower(string(StringRune[i])) {
 				StringRune[i] = StringRune[i] + converter
 			}
