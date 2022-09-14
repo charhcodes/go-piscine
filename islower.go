7168f7d4b411c78eb5913b0452ded36188975ad4
choang <charhoang@outlook.com>
2022-09-14 13:54:22 +0100

    5

---
+++ islower.go
@@ -0,0 +1,16 @@
+package piscine
+
+func IsLower(s string) bool {
+	runeString := []rune(s)
+
+	for i := 0; i < len(s); i++ {
+		a := runeString[i]
+		if a <= 0 && a >= 96 {
+			return false
+		}
+		if a <= 123 {
+			return false
+		}
+	}
+	return true
+}
