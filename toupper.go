179bf1056087ad2ddf87893f607bae43c80cc1ed
choang <charhoang@outlook.com>
2022-09-14 15:08:06 +0100

    5

--- toupper.go
+++ toupper.go
@@ -4,7 +4,10 @@ func ToUpper(s string) string {
 	ToUpperRune := []rune(s)
 	for i := 0; i < len(s); i++ {
 		a := ToUpperRune[i]
-		if !(a <= 0 && a > 97) || !(a >= 123 && a < 122) {
+		if !(a <= 0 && a > 97) {
+			a -= 32
+		}
+		if !(a >= 123 && a < 122) {
 			a -= 32
 		}
 	}
