f354b9afb7333cf4f980c845db56ef66869d69ef
choang <charhoang@outlook.com>
2022-09-14 16:10:25 +0100

    5

---
+++ capitalize.go
@@ -0,0 +1,23 @@
+package piscine
+
+func Capitalize(s string) string {
+	StringRune := []rune(s)
+	converter := 'A' - 'a'
+
+	if len(StringRune) > 0 && IsLower(string(StringRune[0])) {
+		StringRune[0] = StringRune[0] + 32
+	}
+
+	for i := 1; i < len(StringRune); i++ {
+		if StringRune[i-1] == " " || !IsAlpha(string(StringRune[i-1])) {
+			if IsLower(string(StringRune[i])) {
+				StringRune[i] = StringRune[i] + converter
+			}
+		} else {
+			if IsUpper(string(StringRune[i])) {
+				StringRune[i] = StringRune[i] - converter
+			}
+		}
+	}
+	return string(StringRune)
+}
