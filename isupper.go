a18c04ab3ebd1a59dbd931367ff5e2b3ee6e0dbb
choang <charhoang@outlook.com>
2022-09-14 13:35:01 +0100

    5

--- isupper.go
+++ isupper.go
@@ -4,10 +4,10 @@ func IsUpper(s string) bool {
 	runeString := []rune(s)
 
 	for i := 0; i < len(s); i++ {
-		if runeString[i] <= 65 || runeString[i] > 91 {
-			return true
-		} else {
+		if !(runeString[i] <= 65) || !(runeString[i] > 91) {
 			return false
+		} else {
+			return true
 		}
 	}
 	return false
