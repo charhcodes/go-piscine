8464273806b4cce2e87be0c2b5cc31f98e0455ec
choang <charhoang@outlook.com>
2022-09-15 14:59:16 +0100

    6

---
+++ revparams/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args1 := os.Args
+	runes := []rune(args1)
+	emptyRunes := make([]rune, 0)
+
+	// print
+	for i, value := range args1 {
+		if i != 0 {
+			for _, value := range value {
+				emptyRunes = append(emptyRunes, value)
+			}
+		}
+	}
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	for i := 0; i < len(emptyRunes); i++ {
+		z01.PrintRune(emptyRunes[i])
+	}
+	z01.PrintRune('\n')
+}
