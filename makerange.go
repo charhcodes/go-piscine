7621bc4b6192fa9b7e45e57dbbad28c818bd2842
choang <charhoang@outlook.com>
2022-09-18 00:14:14 +0100

    7

--- makerange.go
+++ makerange.go
@@ -5,6 +5,10 @@ func MakeRange(min, max int) []int {
 	for i := range a {
 		for i = min; i < max; i++ {
 			a[i] = min + i
+		} else if max == 9223372036854775807 {
+			return nil
+		} else if min <= max {
+			return nil
 		}
 	}
 	return a
