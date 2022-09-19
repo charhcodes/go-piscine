8200e44831b913b5c92bc4fddb86f52a82421e3e
choang <charhoang@outlook.com>
2022-09-19 16:56:33 +0100

    8

--- boolean/main.go
+++ boolean/main.go
@@ -4,6 +4,8 @@ import (
 	"os"
 
 	"github.com/01-edu/z01"
+
+	"piscine"
 )
 
 func printStr(s string) {
@@ -23,9 +25,37 @@ func isEven(nbr int) bool {
 	}
 }
 
+func TrimAtoi(s string) {
+	counter := 0
+	add := true
+	number := false
+	arr := []int{}
+	for _, val := range s {
+		if '0' <= val && val <= '9' {
+			if val == '0' && !number {
+				continue
+			}
+			arr = append(arr, int(val-48))
+			number = true
+			counter++
+		} else if val == '-' && !number {
+			add = false
+		}
+	}
+	b := 0
+	c := 1
+	for i := counter - 1; i >= 0; i-- {
+		b += arr[i] * c
+		c *= 10
+	}
+	if !add {
+		b *= -1
+	}
+}
+
 func main() {
 	a := os.Args[1:]
-	lengthOfArg := TrimAtoi(a)
+	lengthOfArg := piscine.TrimAtoi(a)
 	EvenMsg := "I have an even number of arguments"
 	OddMsg := "I have an odd number of arguments"
 	if isEven(lengthOfArg) {
