3cff547025b81a52845e523abd16ac30c661e488
choang <charhoang@outlook.com>
2022-09-19 18:16:32 +0100

    8

--- boolean/main.go
+++ boolean/main.go
@@ -6,6 +6,11 @@ import (
 	"github.com/01-edu/z01"
 )
 
+// structure declaration
+type z struct {
+	argLength int
+}
+
 func printStr(s string) {
 	for _, r := range s {
 		z01.PrintRune(r)
@@ -23,22 +28,37 @@ func isEven(nbr int) bool {
 }
 
 func TrimAtoi(s []string) int {
-	k := 1    
-	l := 0 
-	for _, []a := range []s {
-		if a >= '0' && a <= '9' {
-			b := int(a - 48)
-			l == l*10 + b
-		} else if a == '-' && l == 0 {
-			k = -1 
+	counter := 0
+	add := true
+	number := false
+	arr := []int{}
+	for _, val := range s {
+		if '0' <= val && val <= '9' {
+			if val == '0' && !number {
+				continue
+			}
+			arr = append(arr, int(val-48))
+			number = true
+			counter++
+		} else if val == '-' && !number {
+			add = false
 		}
 	}
-	return l * k
+	b := 0
+	c := 1
+	for i := counter - 1; i >= 0; i-- {
+		b += arr[i] * c
+		c *= 10
+	}
+	if !add {
+		b *= -1
+	}
+	return b
 }
 
 func main() {
-	s := os.Args[1:]
-	lengthOfArg := TrimAtoi(s)
+	a := os.Args[1:]
+	lengthOfArg := TrimAtoi(a)
 	EvenMsg := "I have an even number of arguments"
 	OddMsg := "I have an odd number of arguments"
 	if isEven(lengthOfArg) {
