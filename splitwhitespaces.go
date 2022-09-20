f867e5039b2aff7fd0c562eb7037b74e589921ac
choang <charhoang@outlook.com>
2022-09-20 20:29:18 +0100

    7

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,6 +1,6 @@
 package piscine
 
-func SplitWhiteSpaces(s string) []string {
+/*func SplitWhiteSpaces(s string) []string {
 	stringSlice := []string(s)
 	emptyString := []string{}
 
@@ -13,4 +13,22 @@ func SplitWhiteSpaces(s string) []string {
 		}
 	}
 	return emptyString
+}
+*/
+
+func SplitWhiteSpaces(s string) []string {
+	var word []rune
+	var str []string
+	for i, val := range s {
+		if val != 32 {
+			word = append(word, val)
+		}
+		if val == 32 || i == len(s)-1 {
+			if len(word) > 0 {
+				str = append(str, string(word))
+				word = []rune{}
+			}
+		}
+	}
+	return str
 }
