6db526e743d362e1fb0803ebfc6a9012ee5068f4
choang <charhoang@outlook.com>
2022-09-20 16:02:51 +0100

    8

--- point/main.go
+++ point/main.go
@@ -21,12 +21,13 @@ func main() {
 	setPoint(points)
 
 	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
-}
-*/
+}*/
 
-package piscine
+/*package piscine
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type point struct {
 	x int
@@ -44,4 +45,51 @@ func main() {
 	setPoint(points)
 
 	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+}*/
+
+package main
+
+import "github.com/01-edu/z01"
+
+type point struct {
+    x int
+    y int
+}
+
+func main() {
+    ptr := point{
+        x: 1,
+        y: 2,
+    }
+
+    ptr.setPoint()
+
+    printStr("x = ")
+    for , curr := range getDigits(ptr.x) {
+        z01.PrintRune(rune(curr + 48))
+    }
+    printStr(", y = ")
+    for , curr := range getDigits(ptr.y) {
+        z01.PrintRune(rune(curr + 48))
+    }
+    z01.PrintRune('\n')
+}
+
+func printStr(s string) {
+    for _, curr := range s {
+        z01.PrintRune(rune(curr))
+    }
+}
+
+func getDigits(n int) []int {
+    if n < 10 {
+        return []int{n}
+    } else {
+        return append(getDigits(n/10), (n % 10))
+    }
+}
+
+func (d point) setPoint() {
+    (d).x = 42
+    (*d).y = 21
 }
