75233d5429ee3305df7b79e857b1d9f935f22849
choang <charhoang@outlook.com>
2022-09-21 18:00:01 +0100

    9

---
+++ issorted.go
@@ -0,0 +1,28 @@
+package piscine
+
+// return positive int if a > b
+// return 0 if a == b
+// if else return a negative int
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	counter := 0
+	equalCounter := 0
+	for i := 0; i < len(a)-1; i++ {
+		if f(a[i], a[i+1]) > 0 {
+			counter++
+		} else if f(a[i], a[i+1]) < 0 {
+			counter--
+		} else {
+			equalCounter++
+		}
+	}
+	if counter < 0 {
+		counter += -1
+	}
+	counter += equalCounter
+	if counter != len(a)-1 {
+		return false
+	} else {
+		return true
+	}
+}
