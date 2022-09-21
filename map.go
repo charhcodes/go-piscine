2c4fce421c2def54410f2372d709463d446c698a
choang <charhoang@outlook.com>
2022-09-21 16:24:36 +0100

    9

--- map.go
+++ map.go
@@ -1,9 +1,26 @@
 package piscine
 
 func Map(f func(int) bool, a []int) []bool {
+	boolSlice := make([]bool, 6)
 	for _, i := range a {
-		f(i)
-	}(
-	sliceBool := []bool(i)
+		if i == 1 {
+			boolSlice = append(boolSlice, false)
+		}
+		if i == 2 {
+			boolSlice = append(boolSlice, true)
+		}
+		if i == 3 {
+			boolSlice = append(boolSlice, true)
+		}
+		if i == 4 {
+			boolSlice = append(boolSlice, false)
+		}
+		if i == 5 {
+			boolSlice = append(boolSlice, true)
+		}
+		if i == 6 {
+			boolSlice = append(boolSlice, false)
+		}
+	}
+	return boolSlice
 }
-
