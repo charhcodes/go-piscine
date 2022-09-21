5ddc10b47de9f43f43bff3f243fab1757f140fdf
choang <charhoang@outlook.com>
2022-09-21 17:07:43 +0100

    9

--- countif.go
+++ countif.go
@@ -1,9 +1,19 @@
 package piscine
 
+/*
 func CountIf(f func(string) bool, tab []string) int {
 	for _, v := range tab {
 		if f(v) {
 			return len(tab)
 		}
 	}
+} */
+
+func CountIf(f func(string) bool, tab []string) int {
+	for _, v := range tab {
+		if f(v) {
+			return len(tab)
+		}
+	}
+	return len(tab)
 }
