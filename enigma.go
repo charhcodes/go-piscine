e56ab47059358bfe2f3086e8816fb905a0f02108
choang <charhoang@outlook.com>
2022-09-22 21:16:23 +0100

    hackathon

---
+++ enigma.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	pointerA := a***
+	pointerB := *b
+	pointerC := *******c
+	pointerD := ****d
+
+	pointerA = *******c
+	pointerB = a***
+	pointerC = ****d
+	pointerD = *b
+}
