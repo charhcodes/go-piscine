d6e39284f8692cb6d8991035709414ce346105e7
choang <charhoang@outlook.com>
2022-09-22 21:51:03 +0100

    hackathon

---
+++ ispowerof2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+	"strconv"
+)
+
+func main() {
+	for len(os.Args) == 2 { // works only when terminal input = 2
+		numbers, err := strconv.Atoi(os.Args[1]) // strconv takes terminal input (minus program name), returns numbers + error
+		if err != nil {                          // if error status is true aka if error status is not zero (nil acts as a non-bool false)
+			panic(err) // panic(err) checks for errors, basically cancelling the program
+		}
+		newnum := numbers  // reassigning numbers, keeps old numbers value
+		counts := 0        // counter
+		for numbers != 1 { // when numbers not equal to 1
+			if newnum%2 != 0 { // if remainder of newnum not equal to 0
+			} else {
+				newnum = newnum / 2 // newnum divided by 2
+			}
+			counts++ // counter up (after if else statements)
+		}
+		var x int = 2 ^ counts // x = 2 to the power of counter
+		if x == numbers {      // if x equals numbers
+			return
+		}
+	}
+}
