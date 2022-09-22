d98225f537a674ca3cdcc7798e90608ea36b432e
choang <charhoang@outlook.com>
2022-09-22 14:02:54 +0100

    hackathon

--- abort.go
+++ abort.go
@@ -2,7 +2,11 @@ package piscine
 
 func Abort(a, b, c, d, e int) int {
 	medSlice := []int{a, b, c, d, e}
+	Sort(medSlice)
+	return medSlice[2]
+}
 
+func Sort(medSlice []int) {
 	for i := 0; i < len(medSlice); i++ {
 		for j := i; i < len(medSlice); j++ {
 			if medSlice[i] > medSlice[j] {
@@ -10,5 +14,4 @@ func Abort(a, b, c, d, e int) int {
 			}
 		}
 	}
-	return medSlice[2]
 }
