26e2befb7fbd68c00ad816aae9fadcc3b325efa1
choang <charhoang@outlook.com>
2022-09-22 13:52:37 +0100

    hackathon

--- abort.go
+++ abort.go
@@ -2,7 +2,6 @@ package piscine
 
 func Abort(a, b, c, d, e int) int {
 	medSlice := []int{a, b, c, d, e}
-	var newSlice []int
 
 	for i := 0; i < len(medSlice); i++ {
 		for j := i; i < len(medSlice); j++ {
@@ -11,8 +10,7 @@ func Abort(a, b, c, d, e int) int {
 				medSlice[i] = medSlice[j]
 				medSlice[j] = z
 			}
-			newSlice = append(medSlice)
 		}
 	}
-	return newSlice[2]
+	return medSlice[2]
 }
