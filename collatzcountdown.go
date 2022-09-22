a928f283f8e9e44589c3c7c29a2fcdf67df46694
choang <charhoang@outlook.com>
2022-09-22 14:55:25 +0100

    hackathon

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -1,14 +1,20 @@
 package piscine
 
 func CollatzCountdown(start int) int {
-	for i := start; i < len(start); i-- {
+	startSlice := []int{start}
+	counter := 0
+	for i := start; i < start; i-- {
 		if start <= 0 {
 			return -1
-		} else if start[i] % 2 = 0 {
-			start[i] = start[i] / 2  
-		} else {
-			start[i] = start[i] * 3 + 1 
+		} else if start % 2 = 0 {
+			counter++
+			startSlice[i] = startSlice[i] / 2
+		} else if start % 2 = 1 {
+			counter++
+			startSlice[i] = startSlice[i]*3 + 1
+		} else if start[i] == 1 {
+			return
 		}
 	}
-	return i
+	return counter
 }
