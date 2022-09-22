35f939c6e86040561c9cbe6e3774bd7e7c64ea08
choang <charhoang@outlook.com>
2022-09-22 14:04:00 +0100

    hackathon

--- abort.go
+++ abort.go
@@ -9,7 +9,7 @@ func Abort(a, b, c, d, e int) int {
 func Sort(medSlice []int) {
 	for i := 0; i < len(medSlice); i++ {
 		for j := i; i < len(medSlice); j++ {
-			if medSlice[i] > medSlice[j] {
+			if medSlice[j] > medSlice[i] {
 				medSlice[i], medSlice[j] = medSlice[j], medSlice[i]
 			}
 		}
