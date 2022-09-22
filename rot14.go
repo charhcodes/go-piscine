8c163b08c89ec66500c4329c6cb944ba690eed08
choang <charhoang@outlook.com>
2022-09-22 13:17:38 +0100

    hackathon

--- rot14.go
+++ rot14.go
@@ -4,16 +4,12 @@ func Rot14(s string) string {
 	runeString := []rune(s)
 
 	for i := 0; i < len(s); i++ {
+		// A - L inclusive
 		if (runeString[i] >= 65 && runeString[i] <= 76) || (runeString[i] >= 97 && runeString[i] <= 108) {
 			runeString[i] += 14
 		}
-		if (runeString[i] > 76 && runeString[i] < 90) || (runeString[i] > 108 && runeString[i] < 122) {
-			runeString[i] -= 12
-		}
-		if runeString[i] == 90 {
-			runeString[i] -= 12
-		}
-		if runeString[i] == 122 {
+		// M - Z inclusive
+		if (runeString[i] > 76 && runeString[i] <= 90) || (runeString[i] > 108 && runeString[i] <= 122) {
 			runeString[i] -= 12
 		}
 	}
