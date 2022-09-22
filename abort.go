a7bad8f5b89461a749350d2ff942eadbe293afe5
choang <charhoang@outlook.com>
2022-09-22 14:12:33 +0100

    hackathon

--- abort.go
+++ abort.go
@@ -2,13 +2,13 @@ package piscine
 
 func Abort(a, b, c, d, e int) int {
 	medSlice := []int{a, b, c, d, e}
-	Sort(medSlice)
+	SortSlice(medSlice)
 	return medSlice[2]
 }
 
-func Sort(medSlice []int) {
+func SortSlice(medSlice []int) {
 	for i := 0; i < len(medSlice); i++ {
-		for j := 0; i < len(medSlice); j++ {
+		for j := 0; j < len(medSlice); j++ {
 			if medSlice[j] > medSlice[i] {
 				medSlice[i], medSlice[j] = medSlice[j], medSlice[i]
 			}
